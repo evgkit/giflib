db69363e320d6d10e2810bd81d557b7ac668f6e5
evgkit <kitaev.ea@gmail.com>
2016-01-30 13:04:38 +0300

    Add app config

---
+++ src/main/java/ru/evgkit/giflib/AppConfig.java
@@ -0,0 +1,14 @@
+package ru.evgkit.giflib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by e on 30.01.16.
+ */
+@EnableAutoConfiguration
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
